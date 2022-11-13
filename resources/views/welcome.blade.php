1b641a5d140b78bcc6d6307ced93495350968e82
DESKTOP-LKF81KV\Mak Moinee <titus.silver.ph@gmail.com>
2022-11-14 00:23:08 +0800

    update code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,14 +3,13 @@
 
 <head>
     <meta charset="utf-8">
-    <link rel="icon" href="/storage/images/logo.svg" type="image/x-icon">
+    <link rel="icon" href="/storage/images/favicon.ico" type="image/x-icon">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Bukidnon Groove</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/slasher-film" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/roboto" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
@@ -19,15 +18,17 @@
     <header style="background: #afafafc5;padding:15px;">
         <nav class="navbar navbar-expand-lg navbar-light bg-light" style="background: transparent !important;">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#" style="font-family: 'Roboto',sans-serif;">Bukidnon <span
-                        style="margin-top: -20px;"><br>Groove</span></a>
+                <a class="navbar-brand" href="#"
+                    style="font-family: 'Roboto Medium',sans-serif;font-size: 20px;color:#2D2E2E;filter: drop-shadow(0.5px 0.5px 0.5px #2D2E2E);line-height: 18px;">BUKIDNON
+                    <span style="margin-top: -20px;"><br>GROOVE</span></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="margin-left: 100px;">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0"
+                        style="margin-left: 100px;font-family: 'Roboto Medium',sans-serif;color:#2D2E2E;filter: drop-shadow(0.5px 0.5px 0.5px #2D2E2E);">
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
@@ -41,18 +42,235 @@
                             <a class="nav-link" href="#">About</a>
                         </li>
                     </ul>
-                    <form class="d-flex" style="background: #919090c5">
-                        <button class="btn btn-light">Login</button>
-                        <button class="btn btn-primary">Signup</button>
+                    <form class="d-flex" style="background: #afafafc5">
+                        <button class="btn btn-light"
+                            style="background: transparent !important; color:white;">Login</button>
+                        <button class="btn btn-primary" style="margin-left: 20px;">Signup</button>
                     </form>
                 </div>
             </div>
         </nav>
     </header>
-    <section style="background: #afafafc5;width:100%;height: 500px;">
+    <section style="background: #afafafc5;width:100%;height:1150px;">
         <div class="container">
             <div class="row">
                 <img src="/images/landingpageimg.jpg" alt="" srcset="" class="img-responsive">
+            </div>
+
+            <div class="row" style="margin-top: 30px;">
+                <img src="/storage/images/banner.png" alt="" srcset="" class="img-responsive"
+                    style="margin-left: -120px; width: 450px;">
+                <div class="col-md-8">
+                    <h4 style="color:#2D2E2E; font-family: 'Roboto Medium',sans-serif;top: 587px;filter: drop-shadow(0.5px 0.5px 0.5px #2D2E2E);"
+                        class="r0">
+                        THE FASTEST WAY
+                        TO FIND AND BOOK GREAT EMERGING</h4>
+                </div>
+                <br>
+                <div class="col-md-8" style="margin-left: 330px;margin-top: -20px;">
+                    <h1
+                        style="position: absolute;color:#2D2E2E;font-family: 'Roboto Black',sans-serif;filter: drop-shadow(0.5px 0.5px 0.5px #2D2E2E);">
+                        BUKIDNON ARTISTS</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+
+                </div>
+                <div class="col-md-4">
+                    <div class="container" style="margin-top: 150px;">
+                        <div class="row">
+                            <div class="col-xs-1">
+                                <h5 style="color:#2D2E2E;">ARTIST</h5>
+                                <button class="btn btn-outline-secondary rounded-pill p-3 mt-3"
+                                    style="color:#2D2E2E;margin-left: -40px;width: 200px;">SIGNUP AS A
+                                    ARTIST</button>
+                            </div>
+                            <div class="col-xs-1" style="margin-top: -110px;margin-left: 220px; color:#2D2E2E;">
+                                <h5 style="padding-left: 20px;">INDUSTRY</h5>
+                                <button class="btn btn-outline-secondary rounded-pill p-3 mt-3"
+                                    style="color:#2D2E2E; width: 200px;">SIGNUP AS A HOST</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <img src="/storage/images/Vector.png" alt="" srcset="" class="img-responsive"
+                        width="40.75px" height=" 501.89px" style="float: right;margin-top: 350px;">
+                </div>
+            </div>
+
+        </div>
+        <div class="custom" style="width: 400px;">
+            <div class="mbox" style="margin-top: -390px;margin-left: 110px;box-shadow: -2px 0px 0px 0px #2D2E2E;">
+                <br>
+                <h5
+                    style="padding-left:15px;filter: drop-shadow(0.5px 0.5px 0.5px #2D2E2E);font-family: 'Roboto Medium',sans-serif;">
+                    104 ARTIST</h5>
+                <h5
+                    style="padding-left:15px;filter: drop-shadow(0.5px 0.5px 0.5px #2D2E2E);font-family: 'Roboto Medium',sans-serif;">
+                    80 HOST</h5>
+                <h6
+                    style="padding-left:15px;filter: drop-shadow(0.5px 0.5px 0.5px #2D2E2E);font-family: 'Roboto Medium',sans-serif;">
+                    420 GIGS ORGANISED</h6>
+                <br>
+            </div>
+        </div>
+    </section>
+    <section style="background: #F8F8F6;height:800px;">
+        <div class="container">
+            <div class="row" style="padding-top: 150px;">
+                <div class="d-flex justify-content-center">
+                    <img src="/storage/images/favicon.ico" alt="" class="img-responsive" height="100px;">
+                    <h2
+                        style="margin-top: 15px;color:#5F939A;font-family: 'Roboto Medium',sans-serif;filter: drop-shadow(0.5px 0.5px 0.5px #2D2E2E);">
+                        BUKIDNON<br>GROOVE</h2>
+                </div>
+            </div>
+            <div class="row" style="margin-top: 50px;">
+                <div class="d-flex justify-content-center">
+                    <h4 class="text-center" style="color:#2D2E2E;font-family: 'Roboto Light',sans-serif;">Lorem
+                        ipsum dolor sit amet,
+                        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h4>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section style="background: #5F939A;height: 900px;">
+        <div class="container">
+            <div class="row">
+                <div class="d-flex justify-content-center">
+                    <div class="container" style="margin-top: 140px;margin-left: 30px;">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <h1
+                                    style="font-family: 'Roboto Medium',sans-serif; color: #2D2E2E;filter: drop-shadow(0.5px 0.5px 0.5px #2D2E2E);">
+                                    ARTIST</h1>
+                                <hr>
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-xs-1">
+                                            <h1
+                                                style="font-family: 'Roboto Medium',sans-serif; color: #2D2E2E;filter: drop-shadow(0.5px 0.5px 0.5px #2D2E2E);font-size: 80px;">
+                                                01</h1>
+                                        </div>
+                                        <div class="col-xs-2"
+                                            style="margin-top: -90px;margin-left: 110px;box-shadow: -2px 0px 0px 0px #2D2E2E;">
+                                            <h5 style="padding-left:15px;">SET PROFILE</h5>
+                                            <h6 class="text-left"
+                                                style="padding-left:15px;font-family: 'Roboto Thin',sans-serif;filter: drop-shadow(0.5px 0.5px 0.5px #2D2E2E);">
+                                                Lorem
+                                                ipsum dolor sit amet,
+                                                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                                                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                                exercitation ullamco
+                                                laboris nisi ut aliquip ex ea commodo consequat.</h6>
+                                        </div>
+
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-xs-1">
+                                            <h1
+                                                style="float:left;padding-top: 50px;font-family: 'Roboto Medium',sans-serif; color: #2D2E2E;filter: drop-shadow(0.5px 0.5px 0.5px #2D2E2E);font-size: 80px;">
+                                                02</h1>
+                                        </div>
+                                        <div class="col-xs-2"
+                                            style="margin-top: -90px;margin-left: 110px;box-shadow: -2px 0px 0px 0px #2D2E2E;">
+                                            <h5 style="padding-left:15px;">LET VENUE AND HOST FIND YOU</h5>
+                                            <h6 class="text-left"
+                                                style="padding-left:15px;font-family: 'Roboto Thin',sans-serif;filter: drop-shadow(0.5px 0.5px 0.5px #2D2E2E);">
+                                                Lorem
+                                                ipsum dolor sit amet,
+                                                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                                                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                                exercitation ullamco
+                                                laboris nisi ut aliquip ex ea commodo consequat.</h6>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+
+                            </div>
+                            <div class="col-md-4">
+                                <h1
+                                    style="font-family: 'Roboto Medium',sans-serif; color: #2D2E2E;filter: drop-shadow(0.5px 0.5px 0.5px #2D2E2E);">
+                                    HOST</h1>
+                                <hr>
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-xs-1">
+                                            <h1
+                                                style="font-family: 'Roboto Medium',sans-serif; color: #2D2E2E;filter: drop-shadow(0.5px 0.5px 0.5px #2D2E2E);font-size: 80px;">
+                                                01</h1>
+                                        </div>
+                                        <div class="col-xs-2"
+                                            style="margin-top: -90px;margin-left: 110px;box-shadow: -2px 0px 0px 0px #2D2E2E;">
+                                            <h5 style="padding-left:15px;">CREATE</h5>
+                                            <h6 class="text-left"
+                                                style="padding-left:15px;font-family: 'Roboto Thin',sans-serif;filter: drop-shadow(0.5px 0.5px 0.5px #2D2E2E);">
+                                                Lorem
+                                                ipsum dolor sit amet,
+                                                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                                                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                                exercitation ullamco
+                                                laboris nisi ut aliquip ex ea commodo consequat.
+                                        </div>
+                                    </div>
+                                    <div class="row" style="margin-top: 50px;">
+                                        <div class="col-xs-1">
+                                            <h1
+                                                style="font-family: 'Roboto Medium',sans-serif; color: #2D2E2E;filter: drop-shadow(0.5px 0.5px 0.5px #2D2E2E);font-size: 80px;">
+                                                02</h1>
+                                        </div>
+                                        <div class="col-xs-2"
+                                            style="margin-top: -90px;margin-left: 110px;box-shadow: -2px 0px 0px 0px #2D2E2E;">
+                                            <h5 style="padding-left:15px;">CHOOSE</h5>
+                                            <h6 class="text-left"
+                                                style="padding-left:15px;font-family: 'Roboto Thin',sans-serif;filter: drop-shadow(0.5px 0.5px 0.5px #2D2E2E);">
+                                                Lorem
+                                                ipsum dolor sit amet,
+                                                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                                                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                                exercitation ullamco
+                                                laboris nisi ut aliquip ex ea commodo consequat.
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </section>
+    <section style="background: #EDEDED;height: 900px;">
+        <div class="container">
+            <div class="row" style="padding-top: 150px;">
+                <div class="col-md-12">
+                    <h3>TOP ARTIST</h3>
+                    <hr>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+
+                </div>
+                <div class="col-md-4">
+
+                </div>
+                <div class="col-md-4">
+
+                </div>
             </div>
         </div>
     </section>
